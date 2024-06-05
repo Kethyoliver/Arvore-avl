4aa2a20752d94a3d5891407ad0b0df8948cac77c
Kethelyn Ellen <120947841+Kethyoliver@users.noreply.github.com>
2024-06-05 17:58:36 -0300

    Update avl.c

--- Avl - estrutura de dados/avl.c
+++ Avl - estrutura de dados/avl.c
@@ -1,233 +1,229 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int max(int a, int b) { return a > b ? a : b; }
+/*
+Uma árvore binária balanceada é aquela em que, para
+qualquer nó, suas sub-árvores esquerda e direita têm
+a mesma altura
+*/
 
-typedef struct noh
-{
-	int dado;
-	struct noh *esq;
-	struct noh *dir;
-	struct noh *parent;
-	int h;
-} noh_t;
+//estrutura tipo No que compoe a AVL
+typedef struct No {
+  int info, altura;
+  struct No *esquerdo;
+  struct No *direito;
+} tipoAVL;
 
-noh_t *encontre(noh_t *raiz, int dado)
-{
-	if (raiz == NULL)
-	{
-		printf("O elemento %d nao esta na arvore!\n",dado);
-		return NULL;
-	}
-	if (raiz -> dado == dado)
-	{
-		printf("O elemento %d esta na arvore!\n",dado);
-	}
-	if (dado < raiz->dado)
-		return encontre(raiz->esq, dado);
-	else if (dado > raiz->dado)
-		return encontre(raiz->dir, dado);
-	else
-		return raiz;
+//cria novo No
+tipoAVL *criaAVL(int x) {
+  tipoAVL *novo = malloc(sizeof(tipoAVL));
+  novo->altura = 0;
+  novo->info = x;
+  novo->esquerdo = NULL;
+  novo->direito = NULL;
+  return novo;
 }
 
-int altura(noh_t *raiz)
-{
-    return raiz ? raiz->h : 0;
-}
-
-void ajuste_h(noh_t *raiz)
-{
-	raiz->h = 1 + max(altura(raiz->esq), altura(raiz->dir));
-}
-
-noh_t *girarD(noh_t *raiz)
-{
-	noh_t *no = raiz->esq;
-	if (raiz->parent)
-	{
-		if (raiz->parent->esq == raiz) raiz->parent->esq = no;
-		else raiz->parent->dir = no;
-	}
-	no->parent = raiz->parent;
-	raiz->parent = no;
-	raiz->esq = no->dir;
-    if (raiz->esq) raiz->esq->parent = raiz;
-	no->dir = raiz;
-
-
-	ajuste_h(raiz);
-	ajuste_h(no);
-	return no;
-}
-
-noh_t *girarE(noh_t *raiz)
-{
-	noh_t *no = raiz->dir;
-	if (raiz->parent)
-	{
-		if (raiz->parent->dir == raiz) raiz->parent->dir = no;
-		else raiz->parent->esq = no;
-	}
-	no->parent = raiz->parent;
-	raiz->parent = no;
-	raiz->dir = no->esq;
-    if (raiz->dir) raiz->dir->parent = raiz;
-	no->esq = raiz;
-
-
-	ajuste_h(raiz);
-	ajuste_h(no);
-	return no;
-}
-
-noh_t *cria_noh(int dado, noh_t *parent)
-{
-	noh_t *n = malloc(sizeof(noh_t));
-	n->dado = dado;
-    n->parent = parent;
-	n->h = 1;
-	n->esq = NULL;
-	n->dir = NULL;
-	
-	printf("%d Inserido!\n",dado);
-
-	return n;
-}
-
-noh_t *balanceia(noh_t *raiz)
-{
-    if (altura(raiz->esq) - altura(raiz->dir) > 1)
-    {
-        if (altura(raiz->esq->esq) > altura(raiz->esq->dir))
-        {
-            raiz = girarD(raiz);
-        }
-        else
-        {
-            girarE(raiz->esq);
-            raiz = girarD(raiz);
-        }
-    }
-    else if (altura(raiz->dir) - altura(raiz->esq) > 1)
-    {
-        if (altura(raiz->dir->dir) > altura(raiz->dir->esq))
-        {
-            raiz = girarE(raiz);
-        }
-        else
-        {
-            girarD(raiz->dir);
-            raiz = girarE(raiz);
-		}
-    }
-    return raiz;
-}
-
-noh_t *inserir(noh_t *raiz, int dado)
-{
-	int inserido=0;
-    noh_t *atual = raiz;
-    while (atual->dado != dado)
-    {
-        if (dado < atual->dado)
-        {
-            if (atual->esq) 
-            {
-				atual = atual->esq;
-			}
-			else
-            {
-                atual->esq = cria_noh(dado, atual);
-                atual = atual->esq;
-                inserido=1;
-            }
-        }
-        else if (dado > atual->dado)
-        {
-            if (atual->dir) 
-            {
-				atual = atual->dir;
-			}
-            else
-            {
-                atual->dir = cria_noh(dado, atual);
-                atual = atual->dir;
-                inserido = 1;
-            }
-        }
-        else return raiz;
-    }
-    if(dado == atual->dado && inserido == 0)
-    {
-    	printf("O elemento %d ja foi inserido!\n",dado);
-	}
-    
-    do
-    {
-        atual = atual->parent;
-        ajuste_h(atual);
-        atual = balanceia(atual);
-    } while (atual->parent);
-    
-    return atual;
-}
-
-void checkavl(noh_t *raiz)
-{
-	if(altura(raiz->esq) - altura(raiz->dir) > 1 && altura(raiz->dir) - altura(raiz->esq) > 1)
-	{
-		printf("Arvore desbalanceada!\n");		
-	}
-	else
-	{
-		printf("Arvore balanceada!\n");
-	}
-}
-
-void print_avl(noh_t *noh, int elemento)
-{
-    int ix;
-    for (ix = 0; ix < elemento; ix++) printf(" ");
-    if (!noh) printf("Filho vazio\n");
-    else
-    {
-        printf("valor %d h = %d\n", noh->dado, noh->h);
-        print_avl(noh->esq, elemento + 4);
-        print_avl(noh->dir, elemento + 4);
-    }
-}
-
-void print_arvore(noh_t *noh)
-{
-    print_avl(noh, 0);
-}
-
-int main(int argc, char *argv[])
-{
-    noh_t *raiz = cria_noh(1, NULL);
-    raiz = inserir(raiz, 2);
-    raiz = inserir(raiz, 2);
-    raiz = inserir(raiz, 3);
-    raiz = inserir(raiz, 4);
-    raiz = inserir(raiz, 5);
-    raiz = inserir(raiz, 6);
-    raiz = inserir(raiz, 7);
-    raiz = inserir(raiz, 8);
-    raiz = inserir(raiz, 9);
-    raiz = inserir(raiz, 9);
-    raiz = inserir(raiz, 10);
-    raiz = inserir(raiz, 11);
-    raiz = inserir(raiz, 12);
-    raiz = inserir(raiz, 13);
-    encontre(raiz,3);
-	encontre(raiz,19);
-	checkavl(raiz);
-
-	printf("\n\n\n\n");
-
-    	
-    print_arvore(raiz);
-    
+//funcao altura no auxiliar
+int altura(tipoAVL *raiz) {
+  int alt_esq, alt_dir;
+  if (raiz == NULL) {
     return 0;
+  }
+  if (raiz->esquerdo == NULL) {
+    alt_esq = 0;
+  } else {
+    alt_esq = 1 + raiz->esquerdo->altura;
+  }
+  if (raiz->direito == NULL) {
+    alt_dir = 0;
+  } else {
+    alt_dir = 1 + raiz->direito->altura;
+  }
+  if (alt_esq > alt_dir) {
+    return alt_esq;
+  }
+  return alt_dir;
+}
+
+//implementacoes das rotações simples e duplas para etapa de rebalanceamento
+//rotacoes simples: no desbalanceado (pai), seu filho e o seu neto estão todos no mesmo sentido de inclinação
+
+//rotacao simples direita
+tipoAVL *rotaDir(tipoAVL *raiz) {
+  tipoAVL *aux = raiz->esquerdo; //inserindo na sub-árvore da esquerda do filho esq
+  //no desbalanceado
+  //rotacoes a esquerda
+  raiz->esquerdo = aux->direito;
+  aux->direito = raiz;
+  raiz->altura = altura(raiz);
+  aux->altura = altura(aux);
+  printf("Rotacao a direita: ");
+  printf("troca de %d por %d\n", raiz->info, aux->info);
+  return aux;
+}
+
+//rotacao simples esquerda
+tipoAVL *rotaEsq(tipoAVL *raiz) {
+  tipoAVL *aux = raiz->direito;
+  raiz->direito = aux->esquerdo;
+  aux->esquerdo = raiz;
+  raiz->altura = altura(raiz);
+  aux->altura = altura(aux);
+  printf("Rotacao a esquerda: ");
+  printf("troca de %d por %d\n", raiz->info, aux->info);
+  return aux;
+}
+
+//rotacos duplas: no desbalanceado (pai) e seu filho estão inclinados no sentido inverso ao neto - equivale a duas rotações simples.
+
+//rotacao dupla direita
+tipoAVL *rotaDDir(tipoAVL *raiz) {
+  raiz = rotaEsq(raiz);
+  return raiz;
+}
+
+//rotacao dupla esquerda
+tipoAVL *rotaDEsq(tipoAVL *raiz) {
+  raiz = rotaDir(raiz);
+  return raiz;
+}
+
+//rotacao esq-direita
+tipoAVL *rotaEsqDir(tipoAVL *raiz) {
+  raiz->esquerdo = rotaEsq(raiz->esquerdo);
+  raiz = rotaDir(raiz);
+  return raiz;
+}
+
+//rota dir-esq
+tipoAVL *rotaDirEsq(tipoAVL *raiz) {
+  raiz->direito = rotaDir(raiz->direito);
+  raiz = rotaEsq(raiz);
+  return raiz;
+}
+
+//calcula fatorBal
+/*
+FB(n) = altura(sub arvore dir) – altura(sub arvor esq)
+se FB(n) = 0, as duas sub-árvores têm a mesma
+altura
+se FB(n) = -1, a sub-árvore esquerda é mais alta que
+a direita em 1
+se FB(n) = +1, a sub-árvore direita é mais alta que a
+esquerda em 1
+*/
+int fatorBal(tipoAVL *raiz) {
+  int alt_esq, alt_dir;
+  if (raiz == NULL) {
+    return 0;
+  }
+  if (raiz->esquerdo == NULL) {
+    alt_esq = 0;
+  } else {
+    alt_esq = 1 + raiz->esquerdo->altura;
+  }
+  if (raiz->direito == NULL) {
+    alt_dir = 0;
+  } else {
+    alt_dir = 1 + raiz->direito->altura;
+  }
+  return (alt_esq - alt_dir);
+}
+
+//insercao elementos na avl
+tipoAVL *insere(tipoAVL *raiz, int x) {
+  if (raiz == NULL) {
+    raiz = criaAVL(x);
+  } else {
+    if (x > raiz->info) {
+      raiz->direito = insere(raiz->direito, x);
+      if (fatorBal(raiz) == -2) {
+        if (x > raiz->direito->info) {
+          raiz = rotaDDir(raiz);
+        } else {
+          raiz = rotaDirEsq(raiz);
+        }
+      }
+    } else {
+      if (x < raiz->info) {
+        raiz->esquerdo = insere(raiz->esquerdo, x);
+        if (fatorBal(raiz) == 2) {
+          if (x < raiz->esquerdo->info) {
+            raiz = rotaDEsq(raiz);
+          } else {
+            raiz = rotaEsqDir(raiz);
+          }
+        }
+      }
+    }
+  }
+  raiz->altura = altura(raiz);
+  return raiz;
+}
+
+void inOrdem(tipoAVL *raiz) {
+  if (raiz != NULL) {
+    inOrdem(raiz->esquerdo);
+    printf(" %d (%d)\n", raiz->info, fatorBal(raiz));
+    inOrdem(raiz->direito);
+  }
+}
+
+//##################################################
+
+/*---------- EXEMPLO ----------*/
+/*
+a) Mostrar as rotações necessárias para a construção da seguinte árvore AVL: 9, 7, 1, 3, 4 e 8
+*/
+int main() {
+  printf("\n-------------------------------");
+  printf("\nExemplo Árvores Binárias AVL");
+  printf("\n-------------------------------\n");
+
+  //##################################################
+
+  printf(">> EXEMPLO\n");
+
+  //##################################################
+
+  printf("a) Mostrar as rotações necessárias para a construção da seguinte árvore AVL: 9, 7, 1, 3, 4 e 8\n");
+
+  printf("\nALV: 9 7 1 3 4  8");
+  printf("\n\n");
+  tipoAVL *raiz = NULL;
+  raiz = insere(raiz, 9);
+  raiz = insere(raiz, 7);
+  raiz = insere(raiz, 1);
+  raiz = insere(raiz, 3);
+  raiz = insere(raiz, 4);
+  raiz = insere(raiz, 8);
+
+  //##################################################
+  /*
+  b) Defina em C uma estrutura de dados que possa representar uma árvore AVL
+  */
+
+  printf("\nb) Defina em C uma estrutura de dados que possa representar uma árvore AVL\n");
+
+  printf("\ntypedef struct No{\n\tint info, altura;\n\tstruct No *esquerdo;\n\tstruct No *direito;\n} tipoAVL;\n");
+
+  //##################################################
+  /*
+  c)  Implemente procedimento para calcular FB
+  */
+
+  printf("\nc) Implemente procedimento para calcular FB;\n(consta no codigo)\n");
+
+  //##################################################
+  /*
+  d) Implemente procedimento que percorra a árvore e imprima o fator de balanceamento de cada nó em uma ordem infixada com o seguinte formato:\nn(FB), onde n é uma raiz de subárvore e FB o fator de balanceamento
+  */
+  printf("\nd) Implemente procedimento que percorra a árvore e imprima o fator de balanceamento de cada nó em uma ordem infixada com o seguinte formato:\nn(FB), onde n é uma raiz de subárvore e FB o fator de balanceamento.\n");
+  printf("\nExemplo com a arvore 5 4 3 6 7 8\n n(FB):\n----------\n");
+  inOrdem(raiz);
+  //##################################################
+
+  printf("\nf) Implemente os procedimentos de rotação simples (direita e esquerda)\n(consta no codigo)\n");
+  return 0;
 }
